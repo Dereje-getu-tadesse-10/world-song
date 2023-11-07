3b371f1d01cf4c080d7ea93e1a202c58a894616e
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-11-07 19:59:18 +0100

    update: setup .env

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,10 +1,12 @@
-
-// Next.js Edge API Route Handlers: https://nextjs.org/docs/app/building-your-application/routing/router-handlers#edge-and-nodejs-runtimes
+import { prisma } from '@/prisma';
 
 import type { NextRequest } from 'next/server'
 
 export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
-  return new Response(JSON.stringify({ name: 'John Doe' }))
+
+  const songs = await prisma.countrySong.findMany()
+
+  return new Response(JSON.stringify({songs}))
 }
